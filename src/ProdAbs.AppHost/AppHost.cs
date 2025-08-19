fc1867889266f80a1fbaf6643489dfccbf708c38
renan-marcel <renanmarcelpaulinoleite@gmail.com>
2025-08-19 01:32:29 -0300

    Atualiza dependências e configurações do projeto
    - Atualiza `.gitignore` para incluir `FodyWeavers.xsd` e configurações do Gemini CLI.
    - Adiciona novas referências de pacotes em `Directory.Packages.props`, incluindo `Npgsql.EntityFrameworkCore.PostgreSQL` e bibliotecas do Aspire para Kafka e PostgreSQL.
    - Expande a lógica de inicialização em `AppHost.cs` para configurar bancos de dados PostgreSQL, Zookeeper e Kafka, além de adicionar suporte ao Seq para gerenciamento de logs.
    - Atualiza `ProdAbs.AppHost.csproj` com referências para `Aspire.Hosting.Kafka`, `Aspire.Hosting.PostgreSQL` e `Aspire.Hosting.Seq`.

--- src/ProdAbs.AppHost/AppHost.cs
+++ src/ProdAbs.AppHost/AppHost.cs
@@ -1,3 +1,62 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+
+var postgres = builder.AddPostgres("postgres")
+    .WithDataBindMount(source: "../../container-data/postgres/Data")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var pgAdmin = postgres.WithPgAdmin(pgadmin => pgadmin.WithHostPort(9200));
+
+var ged_db = postgres.AddDatabase("ged-db");
+
+var event_store_db = postgres.AddDatabase("event-store-db");
+
+var zookeeper = builder.AddContainer("zookeeper", "confluentinc/cp-zookeeper", "latest")
+    .WithEnvironment("ZOOKEEPER_CLIENT_PORT", "2181")
+    .WithEnvironment("ZOOKEEPER_TICK_TIME", "2000")
+    .WithEnvironment("ZOOKEEPER_SYNC_LIMIT", "2")
+    .WithEnvironment("ZOOKEEPER_AUTOPURGE_PURGE_INTERVAL", "1")
+    .WithEnvironment("ZOOKEEPER_AUTOPURGE_SNAP_RETAIN_COUNT", "3")
+    .WithEnvironment("ZOOKEEPER_MAX_CLIENT_CNXNS", "1000")
+    .WithEndpoint(port: 2181, targetPort: 2181)
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var kafka = builder.AddKafka("kafka")
+    .WithImageRegistry("wurstmeister")
+    .WithImage("kafka", "latest")
+    .WithReferenceRelationship(zookeeper)
+    .WithDataBindMount(
+        source: "../../container-data/kafka/data",
+        isReadOnly: false)
+    .WithEnvironment("KAFKA_CREATE_TOPICS", "Lancamentos:1:1")
+    .WithEnvironment("KAFKA_ZOOKEEPER_CONNECT", "zookeeper:2181")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WaitFor(zookeeper);
+
+var kafkaUI = kafka.WithKafkaUI(kafkaUi =>
+{
+    kafkaUi.WithHostPort(9100);
+});
+ 
+var seq = builder.AddSeq("seq")
+    .WithDataBindMount(
+        source: "../../../container-data/seq/Data",
+        isReadOnly: false)
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithEnvironment("ACCEPT_EULA", "Y");
+
+
+
+builder.AddProject<Projects.ProdAbs_Presentation_Api>("prodabs-api")
+       .WithReference(postgres)
+       .WithReference(kafka)
+       .WithReference(ged_db)
+       .WithReference(event_store_db)
+       .WithReference(kafka)
+       .WithReference(seq)
+       .WaitFor(kafka)
+       .WaitFor(seq)
+       .WaitFor(ged_db)
+       .WaitFor(event_store_db);
+
 await builder.Build().RunAsync();
